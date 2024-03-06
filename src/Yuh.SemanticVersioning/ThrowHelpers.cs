f988d6f18dd498c3551599bc2525644c6fa61262
YuHima <yuhima03@gmail.com>
2024-03-07 02:01:28 +0900

    feat: add a new static class
    Add a new static class: ThrowHelpers.

---
+++ src/Yuh.SemanticVersioning/ThrowHelpers.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Yuh.SemanticVersioning
+{
+    internal static class ThrowHelpers
+    {
+        [DoesNotReturn]
+        internal static void ThrowArgumentException(string? message = null, string? paramName = null, Exception? innerException = null)
+        {
+            throw new ArgumentException(message, paramName, innerException);
+        }
+
+        [DoesNotReturn]
+        internal static void ThrowFormatException(string? message = null, Exception? innerException = null)
+        {
+            throw new FormatException(message, innerException);
+        }
+
+        internal static void ThrowIfArgumentIsNegative(int value, [CallerArgumentExpression(nameof(value))] string? argName = null)
+        {
+#if NET8_0_OR_GREATER
+            ArgumentOutOfRangeException.ThrowIfNegative(value, argName);
+#else
+            if (value < 0)
+            {
+                throw new ArgumentOutOfRangeException(argName, "The value is negative.");
+            }
+#endif
+        }
+
+        internal static void ThrowIfArgumentIsEmpty(string value, [CallerArgumentExpression(nameof(value))] string? argName = null)
+        {
+            if (value.Length == 0)
+            {
+                throw new ArgumentException("The string is empty.", argName);
+            }
+        }
+    }
+}
